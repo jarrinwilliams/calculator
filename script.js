d5f534d294a36fc0fbbd1e71e0289c1f54629855
Jarrin Williams <jarrin.williams@yahoo.com>
2022-03-14 14:36:42 -0500

    finished most of the js, working on styling next

--- script.js
+++ script.js
@@ -1,24 +1,62 @@
-
-const numOpBtn = document.querySelectorAll('.numBtn, .opBtn, .decBtn');
-numOpBtn.forEach((button) => {
+let nums = [];
+let operation = [];
+const numBtn = document.querySelectorAll('.numBtn');
+numBtn.forEach((button) => {
     button.addEventListener('click', function(e) {
         //makes button put the character in the display
-        const display = document.querySelector('.calcDisplay');
+        const displayChar = document.querySelector('.displayChar')
         const char = button.innerHTML;
-        const chars = display.childNodes;
-        if (button.classList.contains('opBtn')) {
-            for (let i = chars.length - 1; i >= 0; i--) {
-                display.removeChild(chars[i]);
-            }
-        } else if (button.classList.contains('numBtn') && display.chi)
-        const span = document.createElement('span');
-        span.textContent = char;
-        span.classList.add('displayChar');
-        display.appendChild(span);
+        if (displayChar.textContent == nums[0] || displayChar.textContent == operation[0] || displayChar.textContent === 'Undefined') {
+            displayChar.textContent = '';
+        }
+        displayChar.textContent += char;
     })
 })
-const nums = [];
-const char = document.querySelector('.displayChar');
+const opBtns = document.querySelectorAll('.opBtn');
+opBtns.forEach((button) => {
+    button.addEventListener('click', function(e) {
+        const displayChar = document.querySelector('.displayChar');
+        if (nums.length === 1 && operation.length === 0) {
+            operation.push(button.innerHTML);
+            displayChar.textContent = button.innerHTML;
+            return
+        } else {
+            nums.unshift(displayChar.textContent);
+        }
+        displayChar.textContent = button.innerHTML;
+        if (nums.length === 2) {
+            const answer = operate(nums[0], operation[0], nums[1]);
+            displayChar.textContent = answer;
+            if (answer === 'Undefined') {
+                nums.splice(0);
+                operation.splice(0);
+            } else {
+                nums.unshift(answer);
+                nums.splice(1);
+                operation.unshift(button.innerHTML)
+                operation.splice(1);
+            }
+        } else {
+        operation.push(button.innerHTML);
+        }
+    })
+})
+const eqBtn = document.querySelector('.eqBtn');
+eqBtn.addEventListener('click', function(e) {
+    const displayChar = document.querySelector('.displayChar');
+    nums.push(displayChar.textContent);
+    const answer = operate(nums[0], operation[0], nums[1]);
+    displayChar.textContent = answer;
+    if (answer === 'Undefined') {
+        nums.splice(0);
+        operation.splice(0);
+        displayChar.textContent = 'Undefined';
+    } else {
+        nums.unshift(answer);
+        nums.splice(1);
+        operation.splice(0);
+    }
+})
 
 
 // function for adding
@@ -38,17 +76,32 @@ const multiply = (numOne, numTwo) => {
 }
 // function for division
 const divide = (numOne, numTwo) => {
-    if (numOne === 0) {
-        const quotient = numOne / numTwo;
+     const quotient = numOne / numTwo;
+     if (numTwo == 0) {
+         return 'Undefined';
+     } else {
         return quotient;
+     }
+}
+//operating function
+const operate = (firstNum, operator, secondNum) => {
+    firstNum = parseFloat(firstNum);
+    secondNum = parseFloat(secondNum);
+    if (operator === '+') {
+       return add(firstNum, secondNum);
+    } else if (operator === '-') {
+        return subtract(firstNum, secondNum);
+    } else if (operator === 'x') {
+        return multiply(firstNum, secondNum);
+    } else if (operator === '÷') {
+        return divide(firstNum, secondNum);
     }
 }
 // clear button to make it clear the div for right now
 const clearBtn = document.querySelector('.clearBtn');
 clearBtn.addEventListener('click', function(e) {
-    const display = document.querySelector('.calcDisplay');
-    const chars = display.childNodes;
-    for (let i = chars.length - 1; i >= 0; i--) {
-        display.removeChild(chars[i]);
-    }
+    nums.splice(0);
+    operation.splice(0);
+    const displayChar = document.querySelector('.displayChar');
+    displayChar.textContent = '';
 })
